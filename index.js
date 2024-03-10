c2f571c36546c6dc56cb773e6b3155fc4fa0f2d3
Andi Weber <adtweber@gmail.com>
2024-03-10 11:28:19 -0400

    added items and item function

--- index.js
+++ index.js
@@ -8,19 +8,13 @@ function newImage(url, left, bottom){
     return object
 }
 
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
@@ -28,3 +22,7 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
